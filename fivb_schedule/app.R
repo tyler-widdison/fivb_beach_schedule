4a8f7d0b069ffc64def093268b28dc24d9367f68
Tyler Widd <tylerperrywiddison@gmail.com>
2022-01-23 22:18:34 -0700

    1-23-2022

---
+++ fivb_schedule/app.R
@@ -0,0 +1,136 @@
+library(shiny)
+library(DT)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(shinyWidgets)
+
+load(file = 'rds/tourn_df.rda')
+load(file = 'rds/match_df.rda')
+
+# wanted tournaments
+tourn <- match_df %>% filter(!TournamentType %in% c('51', '52', '', 'Test')) %>% distinct(TournamentType)
+
+# Define the UI
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(width = 2,
+                 hr(),
+      pickerInput('type', 'Tournament type', choices = sort(tourn$TournamentType), options = list(`actions-box` = TRUE),multiple = T, 
+                  selected = c('GrandSlam',
+                               'MajorSeries',
+                               'Open',
+                               'WorldTour4Star',
+                               'WorldTour5Star')),
+      pickerInput('gender', 'Gender', choices = sort(unique(match_df$Gender)), multiple = T, 
+                  selected = c('M', 
+                               'W'))),
+      mainPanel(width = 10,
+                column(6,
+                       plotOutput('teams')),
+                column(6,
+                       plotOutput('line_plot')),
+                       DT::dataTableOutput('data'))
+    )
+  )
+
+
+
+# Define the server code
+server <- function(input, output) {
+  output$data <- DT::renderDT({
+    DT::datatable(df %>% 
+                    filter(Gender %in% input$gender & Type %in% input$type & !Type %in% c('51', '52', '')) %>% 
+                    select(-No, -Code, -Month, -Title, -EndDateMainDraw) %>% 
+                    rename(`Start date` = StartDateQualification,
+                           `MD teams` = NbTeamsMainDraw,
+                           `Qual teams` = NbTeamsQualification),
+                  rownames = F,
+                  filter = "top",
+                  caption = 'Tournaments played from selected fields',
+                  options = list(dom = 'tlp',
+                                 scrollX = T,
+                                 scrollY="300px",
+                                 pageLength = 400))
+  })
+  
+    
+  output$teams <- renderPlot({
+      dat_team <- df %>%
+        filter(Type %in% input$type & Gender %in% input$gender) %>% 
+        group_by(Season) %>% 
+        summarise(`Main draw` = sum(NbTeamsMainDraw),
+                  `Quali` = sum(NbTeamsQualification),
+                  diff = Quali-`Main draw`,
+                  .groups = 'drop') %>% 
+        pivot_longer(c(`Main draw`:Quali)) %>% 
+        filter(!is.na(Season))
+        
+      mdt <- dat_team %>% 
+        filter(name == 'Main draw')
+      qt <- dat_team %>% 
+        filter(name == 'Quali')
+      
+      dat_team %>% 
+        filter(value != 0) %>% 
+      ggplot() + 
+        #add point range
+        geom_segment(data = mdt, aes(x = value, y = Season, yend = qt$Season, xend = qt$value),
+                     color = "#aeb6bf", size = 4.5, alpha = .5) + 
+        
+        geom_point(aes(x = value, y = Season, color = name), size = 4) +
+        theme(legend.position="bottom") +
+        
+        #color points
+        scale_color_manual(values = c("#009688","#762a83")) +
+
+                #add facets for more control
+        facet_grid(Season ~ ., scales = "free", switch = "y") + 
+        ggtitle("FIVB number of qualification and main draw teams")+
+        theme_minimal() +   
+        theme(panel.grid.major.y = element_blank(),
+              panel.grid.minor.y = element_blank(),
+              panel.grid.major.x = element_blank(),
+              panel.grid.minor.x = element_blank(),
+              axis.title.y = element_blank(),
+              axis.text.y = element_blank(),
+              axis.ticks.y = element_blank(),
+              axis.ticks.x = element_line(color = "#4a4e4d"),
+              strip.text.y.left  = element_text(angle = 0),
+              panel.background = element_rect(fill = "white", color = "white"),
+              strip.background = element_rect(fill = "white", color = "white"),
+              plot.background = element_rect(fill = "white", color = "white"),
+              panel.spacing = unit(0, "lines")) + 
+        labs(x = '# of teams', 
+             color = '')
+      
+    })
+  
+  output$line_plot <- renderPlot({
+    dat_team <- df %>%
+      filter(Type %in% input$type & Gender %in% input$gender) %>% 
+      group_by(Season) %>% 
+      summarise(`Main draw` = sum(NbTeamsMainDraw),
+                `Quali` = sum(NbTeamsQualification),
+                diff = Quali-`Main draw`,
+                .groups = 'drop') %>% 
+      pivot_longer(c(`Main draw`:Quali)) %>% 
+      filter(!is.na(Season))
+    
+    dat_team %>% 
+      ggplot(aes(Season, value, color = name)) + 
+      scale_color_manual(values = c("#009688","#762a83")) +
+      geom_line(size = 1) + 
+      geom_point(size = 2, alpha = .4) +
+      theme_bw() + 
+      labs(x = '',
+           y = '# of teams', 
+           title = 'Qualifiaction teams apperence FIVB beach',
+           caption = "Data: FIVB VIS") + 
+      scale_x_continuous(breaks = seq(2000, 2022, 1)) + 
+      theme(legend.title = element_blank())
+      
+  })
+}
+
+shinyApp(ui = ui, server = server)
---
+++ rds/rds_vis.R
@@ -0,0 +1,75 @@
+library(XML)
+library(dplyr)
+library(tidyr)
+library(textclean)
+
+
+# load rds ----------------------------------------------------------------
+load(file = 'rds/remap.rda')
+load(file = 'rds/type_change.rda')
+
+
+# Tournaments ------------------
+url <- "http://www.fivb.org/vis2009/XmlRequest.asmx?Request=<Requests> <Request Type= 'GetBeachTournamentList' Fields='Type Season No Code Name Title StartDateQualification ENdDateMainDraw Gender NbTeamsMainDraw NbTeamsQualification'></Request></Requests>"
+xml <- XML::xmlParse(url, encoding="UTF-8")
+df <- XML:::xmlAttrsToDataFrame(XML::getNodeSet(xml, path ='//BeachTournament')) %>% 
+  filter(NbTeamsMainDraw != 0) %>% 
+  select(-Version) %>% 
+  arrange(Season) %>% 
+  mutate(Gender = ifelse(Gender == 0, 'M', 'W'),
+         Month = substr(StartDateQualification, 6,7),
+         Month = as.numeric(Month),
+         StartDateQualification = as.Date(StartDateQualification),
+         EndDateMainDraw = as.Date(EndDateMainDraw),
+         Season = as.numeric(Season),
+         NbTeamsMainDraw = as.numeric(NbTeamsMainDraw),
+         NbTeamsQualification = as.numeric(NbTeamsQualification),
+         Title = textclean::replace_non_ascii(Title),
+         Name = textclean::replace_non_ascii(Name))
+
+df <- remap(df, col = 'Type', schema = 'Beach Tournament Type')
+save(df, file = 'rds/tourn_df.rda')
+
+
+# Matches -------------
+match_url <- "http://www.fivb.org/vis2009/XmlRequest.asmx?Request=<Requests> <Request Type= 'GetBeachMatchList' Fields='Season TournamentCode NoPlayerB1 NoPlayerB2 NoPlayerA1 NoPlayerA2 TournamentType RoundName Court TournamentTitle LocalDate LocalTime TeamAFederationCode TeamAName MatchPointsA PointsTeamASet1 PointsTeamASet2 PointsTeamASet3 TeamBFederationCode TeamBName MatchPointsB PointsTeamBSet1 PointsTeamBSet2 PointsTeamBSet3 NbSpectators DurationSet1 DurationSet2 DurationSet3 No NoTournament'></Request></Requests>"
+match_xml <- XML::xmlParse(match_url, encoding="UTF-8")
+match_dfs <- XML:::xmlAttrsToDataFrame(XML::getNodeSet(match_xml, path ='//BeachMatch'))
+#Clean the match data
+match_df <- match_dfs %>% 
+  mutate(TeamAName = replace_non_ascii(TeamAName),
+         TeamBName = replace_non_ascii(TeamBName),
+         TournamentTitle = replace_non_ascii(TournamentTitle),
+         LocalDate = as.Date(LocalDate),
+         Court = as.integer(Court),
+         Hour = as.integer(substr(LocalTime, 1, 2)),
+         Year = as.integer(substr(LocalDate,1,4)),
+         DurationSet1 = as.integer(DurationSet1),
+         DurationSet2 = as.integer(DurationSet2),
+         DurationSet3 = as.integer(DurationSet3)) %>% 
+  select(-Version)
+
+match_df <- remap(match_df, col = 'TournamentType', schema = 'Beach Tournament Type')
+#
+
+# Team finishes -------------
+player_url <- "http://www.fivb.org/vis2009/XmlRequest.asmx?Request=<Requests> <Request Type= 'GetBeachTeamList' Fields='NoPlayer1 NoPlayer2 Name Rank No NoTournament Gender'></Request></Requests>"
+player_xml <- XML::xmlParse(player_url)
+player_df <- XML:::xmlAttrsToDataFrame(XML::getNodeSet(player_xml, path ='//BeachTeam'))
+
+# Attach team finishes to matches -------------------------------------------
+get_gender <- df %>% select(Code, Gender)
+
+team_finishes_pivoted <- player_df %>% pivot_longer(c(NoPlayer1:NoPlayer2))
+match_df <- match_df %>% 
+  pivot_longer(c(NoPlayerB1:NoPlayerA2)) %>% 
+  left_join(team_finishes_pivoted %>% select(Rank, NoTournament, value)) %>% 
+  filter(!is.na(Rank) & Rank != '' & PointsTeamASet1 != '' & PointsTeamBSet1 != '') %>% 
+  pivot_wider(names_from = name, values_from = c('value', 'Rank')) %>% 
+  select(-contains('value'), -c('Rank_NoPlayerB1', 'Rank_NoPlayerA1')) %>% 
+  rename(TeamARank = Rank_NoPlayerA2,
+         TeamBRank = Rank_NoPlayerB2) %>% 
+  left_join(get_gender, by = c('TournamentCode' = 'Code'))
+  
+
+save(match_df, file = 'rds/match_df.rda')
